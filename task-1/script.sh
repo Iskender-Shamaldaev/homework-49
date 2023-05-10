63526148766da8fd8f0964b1eae1b98adf03de8f
Shamaldaev Iskender <sh.iskander012@gmail.com>
2023-05-10 19:16:31 +0600

    script.sh done

---
+++ task-1/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p projects/my_project/directory1/subdirectory1
+mkdir -p projects/my_project/directory2
+echo "Hello from JS" > projects/my_project/directory2/hello.txt
+mkdir -p projects/my_project/directory3/subdirectory2
